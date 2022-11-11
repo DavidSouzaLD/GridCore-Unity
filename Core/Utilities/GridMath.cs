54d3d2c88fb9a2c6241bf9f89e7dee0454e3a2f4
David Allison Silva de Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2022-11-11 18:14:41 -0300

    Update GridMath.cs

--- Core/Utilities/GridMath.cs
+++ Core/Utilities/GridMath.cs
@@ -1,44 +1,55 @@
 using UnityEngine;
+using GridCore;
 
 namespace GridCore.Utilities
 {
-    public static class GridMath
+    public class GridMath : MonoBehaviour
     {
-        public static Vector3Int GetFixedPosition(Vector3Int gridSize, Vector3Int cellSize)
+        public static Vector3Int GetFixedPosition()
         {
-            return ((gridSize * cellSize) / 2) + (cellSize / 2);
+            return ((Settings.gridSize * Settings.cellSize) / 2) + (Settings.cellSize / 2);
         }
 
-        public static Vector3Int GetFixedCellPosition(int x, int y, int z, Vector3Int gridSize, Vector3Int cellSize)
+        public static Vector3Int NativeToWorld(int x, int y, int z)
         {
-            Vector3Int fix = GetFixedPosition(gridSize, cellSize);
-            Vector3Int pos = new Vector3Int(x, y, z) * cellSize;
+            return new Vector3Int(x, y, z) * Settings.cellSize;
+        }
+
+        public static Vector3Int NativeToWorld(Vector3Int position)
+        {
+            return new Vector3Int(position.x, position.y, position.z) * Settings.cellSize;
+        }
+
+        public static Vector3Int GetPivotCell(int x, int y, int z)
+        {
+            Vector3Int fix = GetFixedPosition();
+            Vector3Int pos = new Vector3Int(x, y, z) * Settings.cellSize;
             Vector3Int result = pos - fix;
 
             return result;
         }
 
-        public static Vector3Int GetFixedCellPosition(Vector3Int position, Vector3Int gridSize, Vector3Int cellSize)
+        public static Vector3Int GetPivotCell(Vector3Int position)
         {
-            Vector3Int fix = GetFixedPosition(gridSize, cellSize);
-            Vector3Int pos = position * cellSize;
+            Vector3Int fix = GetFixedPosition();
+            Vector3Int pos = position * Settings.cellSize;
             Vector3Int result = pos - fix;
 
             return result;
         }
-        public static Vector3Int GetFixedCellCenterPosition(int x, int y, int z, Vector3Int gridSize, Vector3Int cellSize)
+        public static Vector3Int GetCenterCell(int x, int y, int z)
         {
-            Vector3Int fix = GetFixedPosition(gridSize, cellSize);
-            Vector3Int pos = (new Vector3Int(x, y, z) * cellSize) + (cellSize / 2);
+            Vector3Int fix = GetFixedPosition();
+            Vector3Int pos = (new Vector3Int(x, y, z) * Settings.cellSize) + (Settings.cellSize / 2);
             Vector3Int result = pos - fix;
 
             return result;
         }
 
-        public static Vector3Int GetFixedCellCenterPosition(Vector3Int position, Vector3Int gridSize, Vector3Int cellSize)
+        public static Vector3Int GetCenterCell(Vector3Int position)
         {
-            Vector3Int fix = GetFixedPosition(gridSize, cellSize);
-            Vector3Int pos = (position * cellSize) + (cellSize / 2);
+            Vector3Int fix = GetFixedPosition();
+            Vector3Int pos = (position * Settings.cellSize) + (Settings.cellSize / 2);
             Vector3Int result = pos - fix;
 
             return result;

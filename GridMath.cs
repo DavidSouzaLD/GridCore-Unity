2045ce7713ec2b2ad172e1cbeb9ecc1130b9bb09
David Allison Silva de Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2022-11-11 04:22:29 -0300

    Create GridMath.cs

---
+++ GridMath.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public static class GridMath
+{
+    public static Vector3Int GetFixedPosition(Vector3Int gridSize, Vector3Int cellSize)
+    {
+        return gridSize * cellSize / 2;
+    }
+
+    public static Vector3Int GetFixedCellPosition(int x, int y, int z, Vector3Int gridSize, Vector3Int cellSize)
+    {
+        Vector3Int fix = GetFixedPosition(gridSize, cellSize);
+        Vector3Int pos = new Vector3Int(x, y, z) * cellSize;
+        Vector3Int result = pos - fix;
+
+        return result;
+    }
+
+    public static Vector3Int GetFixedCellPosition(Vector3Int position, Vector3Int gridSize, Vector3Int cellSize)
+    {
+        Vector3Int fix = GetFixedPosition(gridSize, cellSize);
+        Vector3Int pos = position * cellSize;
+        Vector3Int result = pos - fix;
+
+        return result;
+    }
+}

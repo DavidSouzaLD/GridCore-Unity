9b5ba8d82bc8960f7f33802e58ecf4ed6d0d5b0e
David Allison Silva de Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2022-11-11 14:25:02 -0300

    Update GridMath.cs

--- GridMath.cs
+++ GridMath.cs
@@ -4,7 +4,7 @@ public static class GridMath
 {
     public static Vector3Int GetFixedPosition(Vector3Int gridSize, Vector3Int cellSize)
     {
-        return gridSize * cellSize / 2;
+        return (gridSize * cellSize) / 2;
     }
 
     public static Vector3Int GetFixedCellPosition(int x, int y, int z, Vector3Int gridSize, Vector3Int cellSize)
@@ -20,6 +20,23 @@ public static class GridMath
     {
         Vector3Int fix = GetFixedPosition(gridSize, cellSize);
         Vector3Int pos = position * cellSize;
+        Vector3Int result = pos - fix;
+
+        return result;
+    }
+    public static Vector3Int GetFixedCellCenterPosition(int x, int y, int z, Vector3Int gridSize, Vector3Int cellSize)
+    {
+        Vector3Int fix = GetFixedPosition(gridSize, cellSize);
+        Vector3Int pos = (new Vector3Int(x, y, z) * cellSize) + (cellSize / 2);
+        Vector3Int result = pos - fix;
+
+        return result;
+    }
+
+    public static Vector3Int GetFixedCellCenterPosition(Vector3Int position, Vector3Int gridSize, Vector3Int cellSize)
+    {
+        Vector3Int fix = GetFixedPosition(gridSize, cellSize);
+        Vector3Int pos = (position * cellSize) + (cellSize / 2);
         Vector3Int result = pos - fix;
 
         return result;

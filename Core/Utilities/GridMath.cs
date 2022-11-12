e8ccc33d1a5a048b6a69d4e1572686dba9b4b915
David Allison Silva de Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2022-11-11 22:55:22 -0300

    Update GridMath.cs

--- Core/Utilities/GridMath.cs
+++ Core/Utilities/GridMath.cs
@@ -5,7 +5,12 @@ namespace GridCore.Utilities
 {
     public class GridMath : MonoBehaviour
     {
-        public static Vector3Int GetFixedPosition()
+        public static Vector3Int CellHalf()
+        {
+            return (Settings.cellSize / 2);
+        }
+
+        public static Vector3Int GridHalf()
         {
             return ((Settings.gridSize * Settings.cellSize) / 2) + (Settings.cellSize / 2);
         }
@@ -22,35 +27,35 @@ namespace GridCore.Utilities
 
         public static Vector3Int GetPivotCell(int x, int y, int z)
         {
-            Vector3Int fix = GetFixedPosition();
+            Vector3Int fix = GridHalf();
             Vector3Int pos = new Vector3Int(x, y, z) * Settings.cellSize;
-            Vector3Int result = pos - fix;
+            Vector3Int result = pos - fix + Settings.offset;
 
             return result;
         }
 
         public static Vector3Int GetPivotCell(Vector3Int position)
         {
-            Vector3Int fix = GetFixedPosition();
+            Vector3Int fix = GridHalf();
             Vector3Int pos = position * Settings.cellSize;
-            Vector3Int result = pos - fix;
+            Vector3Int result = pos - fix + Settings.offset;
 
             return result;
         }
         public static Vector3Int GetCenterCell(int x, int y, int z)
         {
-            Vector3Int fix = GetFixedPosition();
+            Vector3Int fix = GridHalf();
             Vector3Int pos = (new Vector3Int(x, y, z) * Settings.cellSize) + (Settings.cellSize / 2);
-            Vector3Int result = pos - fix;
+            Vector3Int result = pos - fix + Settings.offset;
 
             return result;
         }
 
         public static Vector3Int GetCenterCell(Vector3Int position)
         {
-            Vector3Int fix = GetFixedPosition();
+            Vector3Int fix = GridHalf();
             Vector3Int pos = (position * Settings.cellSize) + (Settings.cellSize / 2);
-            Vector3Int result = pos - fix;
+            Vector3Int result = pos - fix + Settings.offset;
 
             return result;
         }
